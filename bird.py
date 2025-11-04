345265dde9f58743034e6cbf1db9e72206f93f6e
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-04 18:52:19 +0900

    새 한마리 프레임 완벽 동작

--- bird.py
+++ bird.py
@@ -23,19 +23,27 @@ class Bird:
         self.xv = throwin_speed
         self.frame = 0
         self.frame_index = 0
+        self.frame_x = 0
+        self.frame_y = 0
         self.face_dir = 1
 
     def draw(self):
+        width = 180
+        height = 160
+        high = 3 * height
+
         if self.face_dir == 1:
-            self.image.clip_composite_draw(int(self.frame) * 180, 170, 185, 150, 0, '', self.x, self.y, 100, 100)
+            self.image.clip_composite_draw(int(self.frame_x) * width, high - (self.frame_y + 1) * height, width, height, 0, '', self.x, self.y, 100, 100)
         else:
-            self.image.clip_composite_draw(int(self.frame) * 180, 170, 185, 150, 0, 'h', self.x, self.y, 100, 100)
+            self.image.clip_composite_draw(int(self.frame_x) * width, high - (self.frame_y + 1) * height, width, height, 0, 'h', self.x, self.y, 100, 100)
 
     def update(self):
         self.x += self.xv * game_framework.frame_time * PIXEL_PER_METER
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTIONS_PER_TIME * game_framework.frame_time) % 14
 
         self.frame_index = int(self.frame)
+        self.frame_x = self.frame_index % 5
+        self.frame_y = self.frame_index // 5
 
         if self.x >= 1600:
             self.face_dir = -1
