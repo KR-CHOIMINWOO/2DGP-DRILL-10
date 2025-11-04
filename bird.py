f95591c0986f32482fa89d2a9b8c68eca47e6ba5
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-04 18:16:00 +0900

    다시 생각해 보니 boy 보다 ball에 가까워서 bird 를 ball 코드 복사함

--- bird.py
+++ bird.py
@@ -1,79 +1,29 @@
-from pico2d import load_image, get_time, load_font
-
+from pico2d import *
 import game_world
 import game_framework
-from ball import Ball
 
+PIXEL_PER_METER = (1.0 / 0.03)  # 1 pixel 3 cm
+GRAVITY = 9.8 # m/s^2 중력 가속도
 
-# Boy의 Run Speed 계산
-PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0            # Km / Hour
-RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
-RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
-RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+class Ball:
+    image = None
 
-# Boy Action Speed
-Time_PER_ACTION = 0.5
-ACTIONS_PER_TIME = 1.0 / Time_PER_ACTION
-FRAMES_PER_ACTION = 8
-FRAME_PER_SECOND = FRAMES_PER_ACTION * ACTIONS_PER_TIME
-
-
-
-
-
-
-
-
-class Idle:
-
-    def __init__(self, bird):
-        self.bird = bird
-
-    def enter(self, e):
-        self.bird.wait_time = get_time()
-        self.bird.dir = 0
-
-
-    def exit(self, e):
-        pass
-
-    def do(self):
-        self.bird.frame = (self.bird.frame + FRAMES_PER_ACTION * ACTIONS_PER_TIME * game_framework.frame_time) % 8
-        if get_time() - self.bird.wait_time > 3:
-            self.bird.state_machine.handle_state_event(('TIMEOUT', None))
+    def __init__(self, x = 400, y = 300, throwin_speed = 15, throwin_angle = 45):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y = x, y
+        self.xv = throwin_speed * math.cos(math.radians(throwin_angle))
+        self.yv = abs(throwin_speed * math.sin(math.radians(throwin_angle)))
 
     def draw(self):
-        if self.bird.face_dir == 1: # right
-            self.bird.image.clip_draw(int(self.bird.frame) * 100, 300, 100, 100, self.bird.x, self.bird.y)
-        else: # face_dir == -1: # left
-            self.bird.image.clip_draw(int(self.bird.frame) * 100, 200, 100, 100, self.bird.x, self.bird.y)
-
-
-
-class Bird:
-    def __init__(self):
-
-        self.item = None
-
-        self.x, self.y = 400, 550
-        self.frame = 0
-        self.face_dir = 1
-        self.dir = 0
-        self.image = load_image('bird_animation.png')
-
-        self.IDLE = Idle(self)
-
-
+        self.image.draw(self.x, self.y)
 
     def update(self):
-        pass
+        #y 축 속도에 중력 가속도 적용
+        self.yv -= GRAVITY * game_framework.frame_time # m/s
 
+        self.x += self.xv * game_framework.frame_time * PIXEL_PER_METER
+        self.y += self.yv * game_framework.frame_time * PIXEL_PER_METER
 
-    def handle_event(self, event):
-        pass
-
-
-    def draw(self):
-        pass
-
+        if self.y < 60:
+            game_world.remove_object(self)
