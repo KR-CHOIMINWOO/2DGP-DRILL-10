0abf4f0e73ed47c010d8e2fd8d8fcc4217bbeac2
CHOIMINWOO <117034384+KR-CHOIMINWOO@users.noreply.github.com>
2025-11-04 18:24:17 +0900

    bird draw 좌우 반전 준비 및 frame과 face_dir 선언

--- bird.py
+++ bird.py
@@ -3,6 +3,15 @@ import game_world
 import game_framework
 
 PIXEL_PER_METER = (1.0 / 0.03)  # 1 pixel 3 cm
+RUN_SPEED_KMPH = 20.0            # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+Time_PER_ACTION = 0.5
+ACTIONS_PER_TIME = 1.0 / Time_PER_ACTION
+FRAMES_PER_ACTION = 8
+FRAME_PER_SECOND = FRAMES_PER_ACTION * ACTIONS_PER_TIME
 
 class Bird:
     image = None
@@ -12,9 +21,14 @@ class Bird:
             Bird.image = load_image('bird_animation.png')
         self.x, self.y = x, y
         self.xv = throwin_speed
+        self.frame = 0
+        self.face_dir = 1
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        if self.face_dir == 1:
+            self.image.clip_composite_draw((int(self.frame) % 8) * 100, 0, 100, 100, 0, '', self.x, self.y, 100, 100)
+        else:
+            self.image.clip_composite_draw((int(self.frame) % 8) * 100, 0, 100, 100, 0, 'h', self.x, self.y, 100, 100)
 
     def update(self):
         self.x += self.xv * game_framework.frame_time * PIXEL_PER_METER
